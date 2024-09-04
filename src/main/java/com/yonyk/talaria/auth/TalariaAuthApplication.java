cd11e40981c61a7d0380100e245f6ad3d1d8d69a
YeonHee Kim <kyh03179@gmail.com>
2024-09-05 08:39:37 +0900

    [SETTING] 스켈레톤 프로젝트 생성

---
+++ src/main/java/com/yonyk/talaria/auth/TalariaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.yonyk.talaria.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalariaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalariaAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yonyk/talaria/auth/TalariaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonyk.talaria.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalariaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
