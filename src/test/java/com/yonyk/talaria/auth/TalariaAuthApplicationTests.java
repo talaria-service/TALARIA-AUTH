cf2d4736bf8f019bd032039435ecc91b466f5274
YeonHee Kim <kyh03179@gmail.com>
2024-09-05 08:56:30 +0900

    [SETTING] build.gradle 의존성 설정, pre-commit 설정, gRPC 설정

--- src/test/java/com/yonyk/talaria/auth/TalariaAuthApplicationTests.java
+++ src/test/java/com/yonyk/talaria/auth/TalariaAuthApplicationTests.java
@@ -6,8 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TalariaAuthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
