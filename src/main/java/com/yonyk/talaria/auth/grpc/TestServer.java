cb416e4152cf9963250b4052f415d139b17d5f1e
YeonHee Kim <kyh03179@gmail.com>
2024-09-07 20:38:50 +0900

    [REFACTOR] gRPC 서비스 정의 수정

--- src/main/java/com/yonyk/talaria/auth/grpc/TestServer.java
+++ src/main/java/com/yonyk/talaria/auth/grpc/TestServer.java
@@ -1,19 +1,15 @@
 package com.yonyk.talaria.auth.grpc;
 
-import com.yonyk.HelloWorldServiceGrpc;
-import com.yonyk.Talaria_Auth.*;
-
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
-public class TestServer extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+public class TestServer extends AuthenticationServiceGrpc.AuthenticationServiceImplBase {
   @Override
-  public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
-    // 비즈니스 로직 구현
-    HelloResponse response =
-        HelloResponse.newBuilder().setText("Hello, " + request.getText()).build();
-    responseObserver.onNext(response);
-    responseObserver.onCompleted();
+  public void getAuthentication(
+      AuthenticationProto.AuthRequest request,
+      StreamObserver<AuthenticationProto.AuthResponse> responseObserver) {
+
+    super.getAuthentication(request, responseObserver);
   }
 }
